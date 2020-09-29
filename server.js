3c091f95bcf1730797df0b44d7ba6f2870c23e42
Enron Hubbard <tygerhardt@gmail.com>
2020-09-29 13:38:56 -0700

    Delete space in server.js.

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-
 app.use(express.static("public"));
-
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
